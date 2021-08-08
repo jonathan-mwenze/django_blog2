d4e5fd79b35dfec44240b658823d352913565cf9
Jonathan mwenze <jonathanmwenze@gmail.com>
2021-08-08 16:14:23 +0200

    Creation des models

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
-# Create your models here.
+class PostCategory(models.Model):
+	name = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.name
+
+class Post(models.Model):
+	title = models.CharField(max_length=50)
+	category = models.ForeignKey(PostCategory, null=True, blank=True, on_delete=models.DO_NOTHING)
+	published = models.BooleanField(default=False)
+	text = models.TextField(blank=True)
+	creation_date = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
+
+class Comment(models.Model):
+	STATUS_VISIBLE = 'visible'
+	STATUS_HIDDEN = 'hidden'
+	STATUS_MODERATED = 'moderated'
+
+	STATUS_CHOICES = (
+        (STATUS_VISIBLE, 'visible'),
+        (STATUS_HIDDEN, 'hidden'),
+        (STATUS_MODERATED, 'moderated'),
+	)
+
+	post = models.ForeignKey(Post, on_delete=models.CASCADE)
+	author = models.CharField(max_length=50)
+	status = models.CharField(max_length=20, default=STATUS_VISIBLE, choices=STATUS_CHOICES)
+	moderation_text = models.CharField(max_length=200, blank=True)
+	created_at = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return '{} - {} - (status={})'.format(self.author, self.text[:20], self.status)
